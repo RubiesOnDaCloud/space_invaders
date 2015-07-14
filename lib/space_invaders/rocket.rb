222d5b1ebc70bd3c94233426ab7f86953a19491a
Erik Michaels-Ober <sferik@gmail.com>
2015-07-14 21:07:54 +0200

    Draw rocket on the screen

--- lib/space_invaders/rocket.rb
+++ lib/space_invaders/rocket.rb
@@ -1,4 +1,15 @@
 module SpaceInvaders
   class Rocket
+    def initialize
+      @image = Gosu::Image.new("media/images/rocket.png")
+    end
+
+    def height
+      @image.height
+    end
+
+    def draw(x, y, z)
+      @image.draw(x, y, z)
+    end
   end
 end
--- lib/space_invaders/window.rb
+++ lib/space_invaders/window.rb
@@ -1,17 +1,23 @@
 require "gosu"
+require "space_invaders/rocket"
 
 module SpaceInvaders
   class Window < Gosu::Window
+    WIDTH = 640
+    HEIGHT = 441
+
     def initialize
-      super(640, 441)
+      super(WIDTH, HEIGHT)
       @background = Gosu::Image.new("media/images/background.jpg")
+      @rocket = Rocket.new
     end
 
     def update
     end
 
     def draw
-      @background.draw(0, 0, 0)
+      @background.draw(0, 0, -1)
+      @rocket.draw(WIDTH / 2, HEIGHT - @rocket.height, 0)
     end
   end
 end
