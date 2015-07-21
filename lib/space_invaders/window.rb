4d3ab98201b7b7edf90941f2dfe9cee338b38c58
Nynne <nynnejc@gmail.com>
2015-07-21 22:00:46 +0200

    added more aliens

--- lib/space_invaders/window.rb
+++ lib/space_invaders/window.rb
@@ -12,7 +12,9 @@ module SpaceInvaders
       @background = Gosu::Image.new("media/images/background.jpg")
       # FIXME: Use the rocket's height instead of hardcoding 60
       @rocket = Rocket.new(WIDTH / 2, HEIGHT - 110, 0, WIDTH-1)
-      @alien = Alien.new(0, 0, 0)
+      @aliens = 10.times.collect do |number_of_times_through_the_loop|
+        Alien.new(60 * number_of_times_through_the_loop, 0, 0)
+      end
     end
 
     def update
@@ -22,13 +24,17 @@ module SpaceInvaders
       if Gosu::button_down?(Gosu::KbRight)
         @rocket.move_right
       end
-      @alien.update
+      @aliens.each do |alien|
+        alien.update
+      end
     end
 
     def draw
       @background.draw(0, 0, -1)
       @rocket.draw
-      @alien.draw
+      @aliens.each do |alien|
+        alien.draw
+      end
     end
   end
 end
