901a4ffce9ec8128e76c9bd5efc651d16226abc9
Sarah Ganter <sa.ganter@web.de>
2015-08-04 20:18:59 +0200

    Add specs for stopping at the edges
    fixes https://github.com/RubiesOnDaCloud/space_invaders/issues/5

--- spec/rocket_spec.rb
+++ spec/rocket_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe SpaceInvaders::Rocket do
       expect(rocket.x).to be(15)
       rocket.move_left
       expect(rocket.x).to be(5)
+    end
+    it "stops at the left edge" do
+      rocket = SpaceInvaders::Rocket.new(9, 0, 0, 500)
+      expect(rocket.x).to be(9)
       rocket.move_left
       expect(rocket.x).to be(0)
-    end
+    end  
   end
   describe "#move_right" do
     it "moves the rocket right by 10 pixel" do
@@ -17,6 +21,11 @@ RSpec.describe SpaceInvaders::Rocket do
       rocket.move_right
       expect(rocket.x).to be(210)
     end
+    it "stops at the right edge" do
+      rocket = SpaceInvaders::Rocket.new(435, 0, 0, 500)
+      expect(rocket.x).to be(435)
+      rocket.move_right
+      expect(rocket.x).to be(444)
+    end
   end
-  describe
 end
